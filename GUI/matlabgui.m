5944ace7ab8099d3fd910ba0749e8702fee6d247
YouriBlom <50104128+YouriBlom@users.noreply.github.com>
2019-05-07 08:57:25 +0200

    Add files via upload

--- GUI/matlabgui.m
+++ GUI/matlabgui.m
@@ -22,7 +22,7 @@ function varargout = matlabgui(varargin)
 
 % Edit the above text to modify the response to help matlabgui
 
-% Last Modified by GUIDE v2.5 02-May-2019 09:10:22
+% Last Modified by GUIDE v2.5 06-May-2019 12:32:37
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -82,29 +82,34 @@ function ReadSensors_Callback(hObject, eventdata, handles)
 global sensorleft
 global sensorright
 global delay
+global time
+tic
 n= 1000;            %number of samples
 duration = 10;      %length of sampling interval
 for i = 1:n
-[sensorleft(i), sensorright(i), delay(i)] = Sensordistance();
+[sensorleft(i), sensorright(i), delay(i)] = getdistance();
+
 set(handles.sensor_left, 'string', sensorleft(i));
 set(handles.sensor_right, 'string', sensorright(i));
 set(handles.delaytime, 'string', delay(i));
-
-input = get(gcf, 'currentkey');
-[new_speed(i), new_direction(i)]=drive(input);
-
-set(handles.speed, 'string', new_speed(i));
-set(handles.direction, 'string', new_direction(i));
-pause(duration/n);
+time(i) = i;
+% speed(i) = str2num(get(handles.speed, 'string'));
+% direction(i) = str2num(get(handles.direction, 'string'));
+% input = get(gcf, 'currentkey');
+% [new_speed(i), new_direction(i)]=drive(speed(i), direction(i), input);
+% %plot(time,sensorleft);
+% set(handles.speed, 'string', new_speed(i));
+% set(handles.direction, 'string', new_direction(i));
+pause(0.01);
 end
-time = [1 : n]*duration/n;
-figure;
-plot(time,sensorleft);
-plot(time,sensorright);
-plot(time,delay);
-xlabel('time(s)')
-ylabel('sensordistance(cm)')
-title('sensormeasurements');
+% time = [1 : n]*duration/n;
+% figure;
+% plot(time,sensorleft);
+% plot(time,sensorright);
+% plot(time,delay); 
+% xlabel('time(s)')
+% ylabel('sensordistance(cm)')
+% title('sensormeasurements');
 
 
 
@@ -113,8 +118,8 @@ function OpenConnection_Callback(hObject, eventdata, handles)
 % hObject    handle to OpenConnection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-Port = '.\\COM10'; %Outgoing COM port 
-result = EPOCommunications('open',Port); %Check the value of result
+Port = '\\.\COM10'; %Outgoing COM port 
+result = EPOCommunications('open',Port) %Check the value of result
 speed = 150;
 direction = 150;
 set(handles.speed, 'string', speed);
@@ -124,65 +129,114 @@ set(handles.direction, 'string', direction);
 function CloseConnection_Callback(hObject, eventdata, handles)
 % hObject    handle to CloseConnection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
+disp(sensorleft)
+disp(sensorright)
 % handles    structure with handles and user data (see GUIDATA)
-result = EPOCommunications('close'); %Check the value of result
+result = EPOCommunications('close') %Check the value of result
 
 
-% --- Executes when entered data in editable cell(s) in sensortable.
-function sensortable_CellEditCallback(hObject, eventdata, handles)
-% hObject    handle to sensortable (see GCBO)
-% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
-%	Indices: row and column indices of the cell(s) edited
-%	PreviousData: previous data for the cell(s) edited
-%	EditData: string(s) entered by the user
-%	NewData: EditData or its converted form set on the Data property. Empty if Data was not changed
-%	Error: error string when failed to convert EditData to appropriate value for Data
+% --- Executes on button press in Drive_button.
+function Drive_button_Callback(hObject, eventdata, handles)
+% hObject    handle to Drive_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+d = (get(handles.editdirection, 'string'))
+s = (get(handles.editspeed, 'string'));
+signal = ['D',d];
+EPOCommunications('transmit',signal);
+signal = ['M',s];
+EPOCommunications('transmit',signal);
+set(handles.speed, 'string', s);
+set(handles.direction, 'string', d);
+
+% --- Executes on button press in Stopbutton.
+function Stopbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to Stopbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+d = int2str(150);
+s = int2str(150);
+signal = ['D',d];
+EPOCommunications('transmit',signal);
+signal = ['M',s];
+EPOCommunications('transmit',signal);
+set(handles.speed, 'string', s);
+set(handles.direction, 'string', d);
 
 
-% --- If Enable == 'on', executes on mouse press in 5 pixel border.
-% --- Otherwise, executes on mouse press in 5 pixel border or over ReadSensors.
-function ReadSensors_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to ReadSensors (see GCBO)
+
+function editspeed_Callback(hObject, eventdata, handles)
+% hObject    handle to editspeed (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% Hints: get(hObject,'String') returns contents of editspeed as text
+%        str2double(get(hObject,'String')) returns contents of editspeed as a double
 
-% --- Executes on button press in up_button.
-function up_button_Callback(hObject, eventdata, handles)
-% hObject    handle to up_button (see GCBO)
+
+% --- Executes during object creation, after setting all properties.
+function editspeed_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to editspeed (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function editdirection_Callback(hObject, eventdata, handles)
+% hObject    handle to editdirection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-speed = str2num(get(handles.speed, 'string'));
-new_speed = speed+5;
-set(handles.speed, 'string', new_speed);
+
+% Hints: get(hObject,'String') returns contents of editdirection as text
+%        str2double(get(hObject,'String')) returns contents of editdirection as a double
 
 
-% --- Executes on button press in right_button.
-function right_button_Callback(hObject, eventdata, handles)
-% hObject    handle to right_button (see GCBO)
+% --- Executes during object creation, after setting all properties.
+function editdirection_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to editdirection (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in Challenge1button.
+function Challenge1button_Callback(hObject, eventdata, handles)
+% hObject    handle to Challenge1button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-direction = str2num(get(handles.direction, 'string'));
-new_direction = direction - 5;;
-set(handles.direction, 'string', new_direction);
+distance = str2num(get(handles.challenge1input, 'string'));
+challenge1(distance);
 
 
-% --- Executes on button press in down_button.
-function down_button_Callback(hObject, eventdata, handles)
-% hObject    handle to down_button (see GCBO)
+
+function challenge1input_Callback(hObject, eventdata, handles)
+% hObject    handle to challenge1input (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-speed = str2num(get(handles.speed, 'string'));
-new_speed = speed-5;
-set(handles.speed, 'string', new_speed);
+
+% Hints: get(hObject,'String') returns contents of challenge1input as text
+%        str2double(get(hObject,'String')) returns contents of challenge1input as a double
 
 
-% --- Executes on button press in left_button.
-function left_button_Callback(hObject, eventdata, handles)
-% hObject    handle to left_button (see GCBO)
+% --- Executes during object creation, after setting all properties.
+function challenge1input_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to challenge1input (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-direction = str2num(get(handles.direction, 'string'));
-new_direction = direction + 5;;
-set(handles.direction, 'string', new_direction);
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end

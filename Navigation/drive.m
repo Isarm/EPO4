74fa553a8cdcd7aff8d613dfed76456e4e6a186d
jasperklp <38658926+jasperklp@users.noreply.github.com>
2019-06-06 15:19:35 +0200

    Add files via upload

--- Navigation/drive.m
+++ Navigation/drive.m
@@ -16,8 +16,14 @@ while(d > 30)
     phicar = atan2((x(end) - x(end - 1)), (y(end) - y(end-1))); %Angle of the direction of the car with x-axis on 0 rad
 
     %Correct steering
-    phi = phitogoal - phicar; % Difference in angle (positive stering to the left is needed and negative correcting by steering right is needed
-
+    phi1 = phitogoal - phicar; % Difference in angle (positive stering to the left is needed and negative correcting by steering right is needed
+    phi2 = phitogoal - phicar + 2*pi;
+    phi3 = phitogoal - phicar - 2*pi;
+    A = [abs(phi1) abs(phi2) abs(phi3); phi1, phi2, phi3];
+    [~,c] = min(angle(A(1,:)
+    phi = A(2,c); %So it takes the angle with the absolute minimum value. This could otherwise lead to an error if the goal is at -0.8pi and the car to 0.8pi. Withouth this correction it would take the angle over 1.6 left instead of the faster 0.4 pad to right
+   
+    
     if d > 30 %Hij stuurt niet meer dichtbij de target
         if phi > 0.15 %Minimale angle voordat ie gaat sturen
         EPOCommunications('transmit','D200')

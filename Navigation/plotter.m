dc2e8c5b9ae05c1f074c2ffcca2b643ee3e4742c
YouriBlom <50104128+YouriBlom@users.noreply.github.com>
2019-06-12 12:24:49 +0200

    Create plotter.m

---
+++ Navigation/plotter.m
@@ -0,0 +1,19 @@
+function plotter(x,y,endpointx, endpointy, interx, intery, wall_x, wall_y)
+    plot(endpointx, endpointy, 'b-x','Markersize',20 ,'Linewidth', 3)
+    hold on
+    plot(interx, intery, 'g-x','Markersize',20 ,'Linewidth', 3)
+    xlim([0 460]);
+    ylim([0 460]);
+    title("Final Challenge")
+    plot(x,y, 'r-o')
+    plot([x(end) interx],[y(end) intery], 'g')
+    distance = num2str(round(sqrt((interx-x(end))^2+(intery-y(end))^2)));
+    xlabel(['Distance (cm)= ' distance], 'Fontsize', 12)
+    for(k = 1:21)
+        circle_x(k)= wall_x+20*cos(2*pi*k/20);
+        circle_y(k)= wall_y+20*sin(2*pi*k/20);
+    end
+    plot(wall_x, wall_y, 'k-x','Markersize',10 ,'Linewidth', 3)
+    plot(circle_x,circle_y, 'k');
+    hold off
+end

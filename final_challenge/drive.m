cfccdd69f9280d362554420e66587c25335d8abb
jasperklp <38658926+jasperklp@users.noreply.github.com>
2019-06-17 21:28:28 +0200

    Add files via upload

--- final_challenge/drive.m
+++ final_challenge/drive.m
@@ -1,7 +1,6 @@
 function drive(goalx, goaly)
-global d k phicar phitogoal z zz; 
-test = 0;
-hardturn = 0.20; %In rad
+global d k phicar phitogoal; 
+hardturn = 0.11; %In rad
 softturn = 0.5 * hardturn; %In rad
 updatelocation(goalx, goaly,0);
 while(d(k) > 30)
@@ -14,43 +13,33 @@ while(d(k) > 30)
     phi = A(2,c); %So it takes the angle with the absolute minimum value, but with this rule it keeps the sign of the value which is needed to know whether you must steer to the right or to the left.
         
     %Send steering signals
-    if test == 1
-        if 0.15 < phi && phi < 0.35  
-            zz = 3;
-        elseif phi > 0.35
-            zz = 5;
-        elseif  -0.15 > phi && phi > -0.35 
-            zz = -3; 
-        elseif phi < -0.35 
-            zz = -5;
-        else
-            zz = 0; 
-        end
-        z = 1;
+    if 0.15 < phi && phi < 0.35  %~5 graden  Minimale angle voordat ie gaat sturen
+        EPOCommunications('transmit','D170')
+        q = 2;
+        phicar = phicar - softturn;
+    elseif phi > 0.35 % ~20 graden
+        EPOCommunications('transmit','D200')
+        q = 2;
+        phicar = phicar - hardturn;
+    elseif  -0.15 > phi && phi > -0.35 %Minimale angle voordat ie gaat sturen
+        EPOCommunications('transmit','D130') 
+        q = 2;
+        phicar = phicar + softturn;
+    elseif phi < -0.35 %Minimale angle voordat ie gaat sturen
+        EPOCommunications('transmit','D100')
+        phicar = phicar + hardturn;
+        q = 2;
     else
-        if 0.15 < phi && phi < 0.35  %~5 graden  Minimale angle voordat ie gaat sturen
-            EPOCommunications('transmit','D170')
-            q = 2;
-            phicar = phicar + softturn;
-        elseif phi > 0.35 % ~20 graden
-            EPOCommunications('transmit','D200')
-            q = 2;
-            phicar = phicar + hardturn;
-        elseif  -0.15 > phi && phi > -0.35 %Minimale angle voordat ie gaat sturen
-            EPOCommunications('transmit','D130') 
-            q = 2;
-            phicar = phicar + softturn;
-        elseif phi < -0.35 %Minimale angle voordat ie gaat sturen
-            EPOCommunications('transmit','D100')
-            phicar = phicar + hardturn;
-            q = 2;
-        else
-            EPOCommunications('transmit','D150')
-            q = 1;
-        end
-        EPOCommunications('transmit','M160')
-        pause(0.5)
-        EPOCommunications('transmit','M150')
+        EPOCommunications('transmit','D150')
+        q = 1;
+    end  
+    EPOCommunications('transmit','M160')
+    pause(0.5)
+    EPOCommunications('transmit','M150')
+    if(phicar > pi)
+        phicar = phicar - 2*pi;
+    elseif(phicar < pi)
+        phicar = phicar + 2*pi;
     end
     updatelocation(goalx, goaly,q)
 end
